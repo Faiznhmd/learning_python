50d6e2fa1f4a070aaeb87b4a878985a966ad9a5d
Faiznhmd <fiaznhmd.0624@gmail.com>
2026-06-13 18:42:58 +0530

    Operators

--- operators.py
+++ operators.py
@@ -1,4 +1,26 @@
+# Operators are special symbols or keywords in Python that perform operations on values or variables.
+
 
 # Arithmetic: + - * / %
 # Comparison: == != > <
 # Logical: and or not
+
+# A. Arithmetic Operators = + - * / %  //
+
+# / division operator returns float value
+# // floor division operator returns integer value
+# % modulus operator returns remainder value
+
+# B. Assignment Operators = += -= *= /= %= //= 
+# C. Comparison Operators = == != > < >= <=
+
+
+# D. Logical Operators = and or not
+# and = True only if both conditions are True  ( and means both conditions must be true.)
+# or = True if at least one condition is True   ( means at least one condition should be true.)
+# not = reverses the result T to F and F to T
+
+
+
+
+# Bitwise Operators
